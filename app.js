a9ab7de23a356bbb37e1aecd84a819a20d38f0f2
James2166 <jlh3ndr@gmail.com>
2022-03-03 15:57:10 -0700

    complete

---
+++ app.js
@@ -0,0 +1,19 @@
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
+}
+
+function clickHandler() {
+    console.log('click event!');   
+}
+
+// listen for the click event
+document.addEventListener('click', clickHandler);
+
+
+waitThreeSeconds();
+console.log('finished execution');
+
+// I dont know if I have to submit all of these, but im going to anyway
