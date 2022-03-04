80332b45b22041d05b8dc3320467a5e83ffd5329
James2166 <jlh3ndr@gmail.com>
2022-03-04 09:18:24 -0700

    complete

---
+++ Starter/app.js
@@ -0,0 +1,21 @@
+var greet = 'Hello!';
+var greet = 'Hola!'; 
+
+console.log(greet);
+
+var english = { 
+    greetings: { 
+        basic: 'Hello!' 
+    } 
+};
+
+// How long is a good break?
+
+var spanish = {};
+
+spanish.greet = 'Hola!';
+
+console.log(english);
+
+// if you've spent most of the day checking these or grading them, Maybe you need to rest for a second
+// I mean there is a TON of material here, dont burn yourself out trying to get it all in one sitting
