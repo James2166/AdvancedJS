db2d89ac6bf9f7f77fa96dfa567420437b2517bd
James2166 <jlh3ndr@gmail.com>
2022-03-08 16:06:28 -0700

    complete

---
+++ Starter/app.js
@@ -0,0 +1,21 @@
+var 
+    // first name of the person (obviously)
+    firstname, 
+    
+    // last name of the person (Again, its obvious)
+    lastname, 
+    
+    // the language (it's right on the nose, just like the last two)
+    // can be 'en' or 'es'
+    language;
+
+var person = {
+    // John Doe is a multi use placeholder name given to people 
+    // who's identity is unknown or being hidden
+    firstname: 'John',
+
+    // Jane Doe is the female Equivalent of John Doe
+    Lastname: 'Doe'
+}
+
+console.log(person)
